71ff57c126fa546153f280c33a237371c4798a17
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-07-19 14:31:22 +0530

    shadcn fix

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -5,8 +5,9 @@ import React, { useEffect, useState } from "react";
 import { Category } from "@prisma/client";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
-import { Command, CommandInput } from "@/components/ui/command";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 import CreateCategoryDialog from "./CreateCategoryDialog";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 interface Props {
     type: TransactionType;
@@ -37,19 +38,45 @@ function CategoryPicker({type}: Props) {
                     variant={"outline"}
                     role="combobox"
                     aria-expanded={open}
-                    className="w-full"
+                    className="ml-5 w-[200px] justify-between"
                 >
                     {selectedCategory ? (
                         <CategoryData category={selectedCategory} />
                     ) : (
                         "Select Category"
                     )}
+                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50"/>
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-[200px] p-0">
                 <Command onSubmit={e => e.preventDefault()}>
                     <CommandInput placeholder="Search Category..."/>
                     <CreateCategoryDialog type={type}/>
+                    <CommandEmpty>
+                        <p>Category Not Found</p>
+                        <p className="text-xs text-muted-foreground">
+                            Create a New category
+                        </p>
+                    </CommandEmpty>
+                    <CommandGroup>
+                        <CommandList>
+                            {
+                                categories && categories.map(
+                                    (category: Category) => (
+                                        <CommandItem key={category.name} onSelect={() => {
+                                            setValue(category.name);
+                                            setOpen(prev => !prev);
+                                        }}>
+                                            <CategoryData category={category}/>
+                                            {/* <Check
+                                                className={`mr-2 w-4 h-4 opacity-0 ${value===category.name && "opacity-100"}`}
+                                            /> */}
+                                        </CommandItem>
+                                    )
+                                )
+                            }
+                        </CommandList>
+                    </CommandGroup>
                 </Command>
             </PopoverContent>
         </Popover>
@@ -58,7 +85,7 @@ function CategoryPicker({type}: Props) {
 
 function CategoryData({category}:{category:Category}) {
     return (
-        <div>
+        <div className="">
             <span role="img">{category.icon}</span>
             <span>{category.name}</span>
         </div>
