d43611f633729b97395b9a68b00ebd2c42891b88
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-07-21 12:28:13 +0530

    transactions page styling and filters pending

--- app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -1,14 +1,52 @@
 "use client";
-import React from "react";
+import { GetTransactionsHistoryResponseType } from "@/app/api/transaction-history/route";
+import { apiConnector } from "@/lib/apiConnector";
+import { dateToUTCDate } from "@/lib/helpers";
+import React, { useEffect, useState } from "react";
 
 interface Props {
     from:Date;
     to:Date;
 }
 
+type TransactionsHistory = GetTransactionsHistoryResponseType[0];
+
 function TransactionTable({from, to}:Props) {
-	return (
-        <div>TransactionTable</div>
+	const [history, setHistory] = useState<GetTransactionsHistoryResponseType[]>([]);
+    const [page, setPage] = useState<number>(0);
+    const getTransactionsHistory = async () => {
+        const res = await apiConnector("GET", `/api/transaction-history?from=${dateToUTCDate(from)}&to=${dateToUTCDate(to)}&page=${page}`, null, null, null);
+        console.log(res.data?.data);
+        setHistory(res.data?.data);
+    }
+    useEffect(() => {
+        getTransactionsHistory();
+    }, [from, to, page]);
+    
+    return (
+        <div>
+            {
+                history.map((item:TransactionsHistory) => {
+                    return (
+                        <div key={item.id}>
+                            {item.categoryIcon} {item.category} {item.type} {item.formattedAmount} {item.date}
+                        </div>
+                    )
+                })
+            }
+            <div className="flex gap-5">
+                <button onClick={() => {
+                    if(page!==0) {
+                        setPage(page-1);
+                    }
+                }}>Prev</button>
+                <button onClick={() => {
+                    if(page<=history.length/5) {
+                        setPage(page+1);
+                    }
+                }}>Next</button>
+            </div>
+        </div>
     );
 }
 
--- app/api/transaction-history/route.ts
+++ app/api/transaction-history/route.ts
@@ -4,59 +4,100 @@ import { AccountQuerySchema } from "@/schemas/account";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-export async function GET(request:Request) {
-    try {
-        const user = await currentUser();
-        if(!user) {
-            redirect("/sign-in");
-        }
-        const {searchParams} = new URL(request.url);
-        const from = searchParams.get("from");
-        const to = searchParams.get("to");
-        const queryParams = AccountQuerySchema.safeParse({from, to});
-        if(!queryParams.success) {
-            return Response.json({
-                error: queryParams.error.message,
-            }, {status: 400});
-        }
-        const transactions = await getTransactionsHistory(user.id, queryParams.data.from, queryParams.data.to);
-        return Response.json({
-            data: transactions,
-        });
-    } catch(err) {
-        console.log(err);
-        return Response.json({
-            error: err,
-        });
-    }
+export async function GET(request: Request) {
+	try {
+		const user = await currentUser();
+		if (!user) {
+			redirect("/sign-in");
+		}
+
+		const { searchParams } = new URL(request.url);
+		const from = searchParams.get("from");
+		const to = searchParams.get("to");
+		const page = parseInt(searchParams.get("page") || "0");
+		const pageSize = parseInt(searchParams.get("pageSize") || "5");
+
+		const queryParams = AccountQuerySchema.safeParse({ from, to });
+		if (!queryParams.success) {
+			return Response.json(
+				{
+					error: queryParams.error.message,
+				},
+				{ status: 400 }
+			);
+		}
+
+		const { transactions, totalTransactions } =
+			await getTransactionsHistory(
+				user.id,
+				queryParams.data.from,
+				queryParams.data.to,
+				page,
+				pageSize
+			);
+
+		return Response.json({
+			data: transactions,
+			totalTransactions,
+		});
+	} catch (err) {
+		console.log(err);
+		return Response.json({
+			error: err,
+		});
+	}
 }
 
-export type GetTransactionsHistoryResponseType = Awaited<ReturnType<typeof getTransactionsHistory>>;
+export type GetTransactionsHistoryResponseType = Awaited<
+	ReturnType<typeof getTransactionsHistory>
+>;
 
-async function getTransactionsHistory(userId:string, from:Date, to:Date) {
-    const userSettings = await prisma.userSettings.findUnique({
-        where: {
-            userId,
-        },
-    });
-    if(!userSettings) {
-        throw new Error("User Settings Not found");
-    }
-    const transactions = await prisma.transactions.findMany({
-        where: {
-            userId,
-            date: {
-                gte: from,
-                lte: to,
-            }
-        },
-        orderBy: {
-            date: "desc",
-        },
-    });
+async function getTransactionsHistory(
+	userId: string,
+	from: Date,
+	to: Date,
+	page: number,
+	pageSize: number
+) {
+	const userSettings = await prisma.userSettings.findUnique({
+		where: {
+			userId,
+		},
+	});
+	if (!userSettings) {
+		throw new Error("User Settings Not found");
+	}
 
-    return transactions.map((transaction) => ({
-        ...transaction,
-        formattedAmount: formatCurrency(transaction.amount),
-    }));
+	const transactions = await prisma.transactions.findMany({
+		where: {
+			userId,
+			date: {
+				gte: from,
+				lte: to,
+			},
+		},
+		orderBy: {
+			date: "desc",
+		},
+		skip: page * pageSize,
+		take: pageSize,
+	});
+
+	const totalTransactions = await prisma.transactions.count({
+		where: {
+			userId,
+			date: {
+				gte: from,
+				lte: to,
+			},
+		},
+	});
+
+	return {
+		transactions: transactions.map((transaction) => ({
+			...transaction,
+			formattedAmount: formatCurrency(transaction.amount),
+		})),
+		totalTransactions,
+	};
 }
