2bf513c6c0fb35df2a38a99872dfd70369ff00ac
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-07-22 11:03:45 +0530

    routes protected using middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+	"/", "/transactions", "/settings"
+]);
+
+export default clerkMiddleware((auth, req) => {
+	if (isProtectedRoute(req)) {
+		auth().protect(); // Protect the route if it matches the defined criteria
+	}
+});
 
 export const config = {
 	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
